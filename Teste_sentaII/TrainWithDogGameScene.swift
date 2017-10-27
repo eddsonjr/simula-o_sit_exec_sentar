1422d89b02b4dbfb81c4c3dd81900965587ca99e
Edson Jr <eddson.jr@gmail.com>
2017-10-27 17:20:00 -0400

    Criando uma barra de progresso feita em spritekit com skshape.
    Signed-off-by: Edson Jr <eddson.jr@gmail.com>

--- Teste_sentaII/TrainWithDogGameScene.swift
+++ Teste_sentaII/TrainWithDogGameScene.swift
@@ -16,8 +16,13 @@ class TrainWithDogGameScene: SKScene {
     
     var ossoVazado1: SKSpriteNode?
     var barraProgresso: SKSpriteNode?
-    
     var ossosRestantes: Int = 3
+    
+    private static var jaIniciado = false
+    
+    
+    let progressBarOutline = SKShapeNode()
+    var insideShapeProgressBar = SKShapeNode()
     
     
     override func didMove(to view: SKView){
@@ -28,9 +33,23 @@ class TrainWithDogGameScene: SKScene {
         self.ossoVazado1 = self.childNode(withName: "ossoVazado1") as? SKSpriteNode
         self.barraProgresso = self.childNode(withName: "barraProgresso") as? SKSpriteNode
         
-        //Verifica as condicoes do progresso de treinamento para configurar a barra de progresso
-        verificarAndamentoDoProcessoDeTreinamento()
        
+        
+        
+        
+        if !TrainWithDogGameScene.jaIniciado {
+            //Teste - Criando a barra de progresso
+            configurarProgressBar()
+        }else {
+            //Verifica as condicoes do progresso de treinamento para configurar a barra de progresso
+            verificarAndamentoDoProcessoDeTreinamento()
+        }
+        
+        
+        
+        
+        
+
     
     }
     
@@ -69,7 +88,8 @@ class TrainWithDogGameScene: SKScene {
             
             if (self.ossoVazado1?.contains(location))!{
                 print("Osso vazado 1 tocado")
-                atualizarBarraProgresso(crescimento: 25)
+                //atualizarBarraProgresso(crescimento: 25)
+                updateProgressBar(progress: CGFloat(15))
             }
         }
     }
@@ -89,7 +109,7 @@ class TrainWithDogGameScene: SKScene {
         
         
         //Se o progresso ja chegou em determinada porcentagem o treinamento estava ocorrendo sem voz
-        // carregar a cena anterior e setar o treinamento para comecar com voz
+         //carregar a cena anterior e setar o treinamento para comecar com voz
         if SentaHelper.porcentagemGeralDoPrgoresso == 45 && SentaHelper.estagioTreinamento == SentatrainStage.somenteComPetisco.rawValue{
             print("[TELA DE PROGRESSO]: Adicionar comando de voz")
             SentaHelper.estagioTreinamento = SentatrainStage.somenteComVoz.rawValue
@@ -97,7 +117,7 @@ class TrainWithDogGameScene: SKScene {
             return
 
         }
-        
+ 
         //Se o progresso ja chegou em <%> e ja houve o treinamento com voz, entao chamar a tela
         //anterior e treinar com voz e gesto
         if SentaHelper.porcentagemGeralDoPrgoresso == 90 && SentaHelper.estagioTreinamento == SentatrainStage.somenteComVoz.rawValue {
@@ -107,7 +127,7 @@ class TrainWithDogGameScene: SKScene {
             chamarCenaAnterior()
             return
         }
-        
+
 
         
         
@@ -157,6 +177,13 @@ class TrainWithDogGameScene: SKScene {
     //e configuracoes dos elementos na tela.
     func verificarAndamentoDoProcessoDeTreinamento() {
         
+        
+        if SentaHelper.porcentagemGeralDoPrgoresso > 0 {
+            
+        }
+        
+        
+        
         //Se a porcentagem de treinamento é zero, entao a barra de progresso nao existe ou esta vazia
         if SentaHelper.porcentagemGeralDoPrgoresso == 0{
             self.barraProgresso?.alpha = 0.0
@@ -164,11 +191,56 @@ class TrainWithDogGameScene: SKScene {
             self.barraProgresso?.alpha = 1.0
             self.barraProgresso?.size = CGSize(width: Int(SentaHelper.tamanhoBarraDeProgresso), height: 35)
         }
-        
-        
-        
-        
     }
+    
+    
+    func configurarProgressBar() {
+        
+        
+        //Outline da barra de progresso
+        progressBarOutline.path = UIBezierPath(roundedRect: CGRect(x: frame.minX + 10, y: -128, width: 500, height: 24), cornerRadius: 4).cgPath
+        progressBarOutline.fillColor = UIColor.clear
+        progressBarOutline.strokeColor = UIColor.black
+        progressBarOutline.lineWidth = 2
+        
+        
+        //Preenchimento da barra de progresso
+        insideShapeProgressBar.path = UIBezierPath(roundedRect: CGRect(x: progressBarOutline.frame.minX+3, y: progressBarOutline.frame.minY-1, width: 0, height: progressBarOutline.frame.height-2), cornerRadius: 1).cgPath
+        insideShapeProgressBar.fillColor = UIColor.purple
+        insideShapeProgressBar.strokeColor = UIColor.clear
+        insideShapeProgressBar.lineWidth = 2
+        
+        
+        addChild(progressBarOutline)
+        progressBarOutline.addChild(insideShapeProgressBar)
+    }
+    
+    
+    
+    
+    
+    
+    func updateProgressBar(progress: CGFloat) {
+        var montante = (self.insideShapeProgressBar.frame.width + progress)
+        
+        if (montante >= self.progressBarOutline.frame.width) {
+            print("Montante: \(montante) | Completado: \(self.insideShapeProgressBar.frame.width) | Progresso: \(progress)")
+            let novoMontante = self.progressBarOutline.frame.width - self.insideShapeProgressBar.frame.width + self.insideShapeProgressBar.frame.width - 6
+            
+            self.insideShapeProgressBar.path = UIBezierPath(roundedRect: CGRect(x: progressBarOutline.frame.minX+3, y: progressBarOutline.frame.minY+3, width: novoMontante, height: progressBarOutline.frame.height-6), cornerRadius: 1).cgPath
+            
+            
+        }else {
+             self.insideShapeProgressBar.path = UIBezierPath(roundedRect: CGRect(x: progressBarOutline.frame.minX+3, y: progressBarOutline.frame.minY+3, width: montante, height: progressBarOutline.frame.height-6), cornerRadius: 1).cgPath
+        }
+        
+        SentaHelper.porcentagemGeralDoPrgoresso = SentaHelper.porcentagemGeralDoPrgoresso + 15
+        print("Porcentagem ja adquirida: \(SentaHelper.porcentagemGeralDoPrgoresso)")
+
+    }
+    
+    
+    
     
 
 
