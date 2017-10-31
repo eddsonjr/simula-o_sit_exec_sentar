1f20043390ce8717d12addceddeb5a5deb3b054c
Edson Jr <eddson.jr@gmail.com>
2017-10-31 15:17:11 -0400

    Tentando criar uma extension para animar uma sequencia de sprites e permitir a execucao de uma closure apos isso.
    Signed-off-by: Edson Jr <eddson.jr@gmail.com>

--- Teste_sentaII/SKSpriteNode+Extensions.swift
+++ Teste_sentaII/SKSpriteNode+Extensions.swift
@@ -14,22 +14,5 @@ import UIKit
 extension SKSpriteNode {
 
 
-    /*Esta funcao serve para se o ponto central de um sprite node intercepta o ponto central de outro node*/
     
-    func interceptaCentro(outroNode: SKSpriteNode) -> Bool{
-        let myCenter = CGPoint(x: self.position.x/2, y: self.position.y/2)
-        let outroCenter = CGPoint(x: outroNode.position.x/2, y: outroNode.position.y/2)
-        
-        print("\(myCenter) | \(outroCenter)")
-        
-        if myCenter == outroCenter {
-            return true
-        }else {
-            return false
-        }
-        
-    
-    
-    }
-
 }
